a75556409acd3c4d7f3ab81e778697b8850529df
shafeeq <eshafeeqe@gmail.com>
2018-04-19 12:14:52 +0800

    Add videos
    4 vidoes added
    -- two cases (Solid and Liquid)
    -- two videos per each cases ( Side-view and 3D-view)

--- matlab/simpleVis.m
+++ matlab/simpleVis.m
@@ -5,18 +5,30 @@ clc;
 
 addpath(genpath('..'));
 
-path = '/home/aslab/Desktop/hackthon/opencl-sph/positions_fl/';
-
+video = VideoWriter('fluid_3Dview.avi','Motion JPEG AVI');
+open(video)
+path = '/home/aslab/Desktop/hackthon/opencl-sph/output/fluid/';
+fig1 = figure('units','normalized','outerposition',[0 0 0.5 1]);
 pause on;
 
-for i =1:2000
-    pause(0.01);
-    M = csvread(strcat(path, 'position_',num2str(i),'.csv'));
+for i =1:500
+    %pause(0.01);
+    M = csvread(strcat(path, 'position_',num2str(i-1),'.csv'));
     noOfParticles = sum(M(:,1)~=0 & M(:,2)~=0 & M(:,3)~=0);
     M = M(1:noOfParticles,1:3);
-    scatter3(M(:,1), M(:,2), M(:,3), 'filled');
+    size_m = size(M,1);
+    size_v = ones(size_m,1)*500;
+    depth = sqrt((-2 - M(:,1)).^2 +  (-2 - M(:,2)).^2 );
+    scatter3(M(:,1), M(:,2), M(:,3),size_v, depth, 'filled');
     axis([-2 2 -2 2 -2 2]);
-    %view(0,0)
-
+    %view(0,0);
+    grid off;
+    set(gca,'XTickLabel',[])
+    set(gca,'YTickLabel',[])
+    set(gca,'ZTickLabel',[])
     drawnow;
+    image = getframe(gcf);
+    writeVideo(video,image.cdata);   
 end
+
+close(video);
